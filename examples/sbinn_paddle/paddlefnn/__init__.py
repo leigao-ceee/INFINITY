8df10910c3a9570f0fe11d44180ba8a55e12d736
simonsLiang <63990611+simonsLiang@users.noreply.github.com>
2022-05-27 13:05:14 +0800

    Delete examples/sbinn_paddle directory

--- examples/sbinn_paddle/paddlefnn/__init__.py
+++
@@ -1,9 +0,0 @@
-# code was heavily based on https://github.com/lululxvi/deepxde
-# Users should be careful about adopting these functions in any commercial matters.
-# https://github.com/lululxvi/deepxde#license
-
-
-
-__all__ = ["FNN"]
-
-from .fnn import FNN
--- examples/sbinn_paddle/paddlegeometry/__init__.py
+++
@@ -1,3 +0,0 @@
-from .geometry_1d import Interval
-from .sampler import sample
-from .timedomain import TimeDomain
