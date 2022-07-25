5670a46081dbf1dbdf9e549ce5d5fb8db80acc65
limin2021 <1121099234@qq.com>
2022-07-25 02:33:01 +0000

    optimize paddleput_along_axis when lenarr >= lenindices.

--- examples/PaddleMD/paddlemd/forces.py
+++ examples/PaddleMD/paddlemd/forces.py
@@ -56,6 +56,7 @@ def paddleput_alone_axis(arr, indices, value, axis, reduce="add"):
         lenarr = arr.shape[0]
         lenindices = indices.shape[0]
     xs = lenarr - lenindices
+    '''
     if xs >= 1:
         newindices = paddle.concat(
             [indices, paddle.zeros([xs], dtype=paddle.int64)]
@@ -65,6 +66,8 @@ def paddleput_alone_axis(arr, indices, value, axis, reduce="add"):
         newindices = indices.reshape([-1, 1])
         newvalue = value
     out = paddle.put_along_axis(arr, newindices, newvalue, axis, reduce=reduce)
+    '''
+    out = paddle.put_along_axis(arr, indices.reshape([-1, 1]), value, axis, reduce=reduce)
     return out
 
 
